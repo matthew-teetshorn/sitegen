3c8aa5202ebb7619c6588f988e99bbf94fb70c74
matthew-teetshorn <matthew.teetshorn@gmail.com>
2025-11-12 18:29:09 -0600

    Infrastructure and TextNode class

---
+++ src/textnode.py
@@ -0,0 +1,26 @@
+from enum import Enum
+
+class TextType(Enum):
+    TEXT = "text"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
+
+class TextNode():
+    def __init__(self, text : str, text_type : TextType, url : str = None):
+        self.text = text
+        self.type = text_type
+        self.url = url
+
+    def __eq__(self, other):
+        equal = True
+        for attrname, attrvalue in vars(self).items():
+            if self.attrname != other.attrname:
+                equal = False
+        return equal
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.type.value}, {self.url})"
+
