0b310b122642d734773595e21d5f7300a69f91d7
matthew-teetshorn <matthew.teetshorn@gmail.com>
2025-11-15 22:47:49 -0600

    Finish adding img and link splitting

---
+++ src/extractlinks.py
@@ -0,0 +1,121 @@
+import re
+from textnode import TextType, TextNode
+from htmlnode import HTMLNode
+
+# Find text items of the form '[link text](linkURL)'
+# Screens for invalid characters in URL
+REGEX_MARKDOWN_LINK = r'(?<!!)\[([^\[\]]+)\]\(([^"()\s{}<>|\\`[\]]+)\)'
+# Find text items of the form '![alt text](linkToImage)'
+REGEX_MARKDOWN_IMG = r'!\[([^[\]]+)\]\(([^"()\s{}<>|\\`[\]]+)\)'
+
+# Tag representing regex matches which have been removed from a substrings
+# used for splitting substrings
+REMOVED_MD = "<R>"
+
+
+def extract_markdown_images(text: str) -> list[tuple[str, str]]:
+    matches = re.findall(REGEX_MARKDOWN_IMG, text)
+    return matches
+
+
+def extract_markdown_links(text: str) -> list[tuple[str, str]]:
+    matches = re.findall(REGEX_MARKDOWN_LINK, text)
+    return matches
+
+
+def split_nodes_image(
+    old_nodes: list[TextNode | HTMLNode],
+) -> list[TextNode | HTMLNode]:
+    new_nodes = []
+    for current_node in old_nodes:
+        # Pass on nodes of other types
+        if not isinstance(current_node, TextNode):
+            new_nodes.append(current_node)
+            continue
+        # Pass on TextNodes of incorrect TextType
+        if current_node.type != TextType.TEXT:
+            new_nodes.append(current_node)
+            continue
+
+        current_img_nodes = extract_markdown_images(current_node.text)
+        img_idx = 0
+
+        # Pass on node with no img markdown tags
+        if not current_img_nodes:
+            new_nodes.append(current_node)
+            continue
+
+        # Passing on count from re.subn as we have already verified images exist
+        no_images_string, _ = re.subn(REGEX_MARKDOWN_IMG, REMOVED_MD, current_node.text)
+        substrings = no_images_string.split(REMOVED_MD)
+        inserted_text = False  # Did we just insert a standard text node?
+
+        for current_substring in substrings:
+            if current_substring == "":  # Next insert should be image
+                inserted_text = True
+                continue
+            if inserted_text:
+                alt_text = current_img_nodes[img_idx][0]
+                url = current_img_nodes[img_idx][1]
+                new_nodes.append(TextNode(alt_text, TextType.IMAGE, url))
+                img_idx += 1
+
+            new_nodes.append(TextNode(current_substring, TextType.TEXT))
+            inserted_text = True
+
+        # Could have an extra image node left over
+        if img_idx < len(current_img_nodes):
+            alt_text = current_img_nodes[img_idx][0]
+            url = current_img_nodes[img_idx][1]
+            new_nodes.append(TextNode(alt_text, TextType.IMAGE, url))
+
+    return new_nodes
+
+
+def split_nodes_link(
+    old_nodes: list[TextNode | HTMLNode],
+) -> list[TextNode | HTMLNode]:
+    new_nodes = []
+    for current_node in old_nodes:
+        # Pass on nodes of other types
+        if not isinstance(current_node, TextNode):
+            new_nodes.append(current_node)
+            continue
+        # Pass on TextNodes of incorrect TextType
+        if current_node.type != TextType.TEXT:
+            new_nodes.append(current_node)
+            continue
+
+        current_link_nodes = extract_markdown_links(current_node.text)
+        img_idx = 0
+
+        # Pass on node with no img markdown tags
+        if not current_link_nodes:
+            new_nodes.append(current_node)
+            continue
+
+        # Passing on count from re.subn as we have already verified images exist
+        no_links_string, _ = re.subn(REGEX_MARKDOWN_LINK, REMOVED_MD, current_node.text)
+        substrings = no_links_string.split(REMOVED_MD)
+        inserted_text = False  # Did we just insert a standard text node?
+
+        for current_substring in substrings:
+            if current_substring == "":  # Next insert should be image
+                inserted_text = True
+                continue
+            if inserted_text:
+                anchor_text = current_link_nodes[img_idx][0]
+                url = current_link_nodes[img_idx][1]
+                new_nodes.append(TextNode(anchor_text, TextType.LINK, url))
+                img_idx += 1
+
+            new_nodes.append(TextNode(current_substring, TextType.TEXT))
+            inserted_text = True
+
+        # Could have an extra link node left over
+        if img_idx < len(current_link_nodes):
+            alt_text = current_link_nodes[img_idx][0]
+            url = current_link_nodes[img_idx][1]
+            new_nodes.append(TextNode(alt_text, TextType.LINK, url))
+
+    return new_nodes
---
+++ src/test_split_imgs_links.py
@@ -0,0 +1,145 @@
+import unittest
+import os
+
+from textnode import TextNode, TextType
+from extractlinks import split_nodes_image, split_nodes_link
+
+
+class TestSplitImgsLinks(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        """
+        Custom setUpClass method to print the current file name.
+        """
+        print(f"\nRunning tests from: {os.path.basename(__file__)}")
+
+    def test_link(self):
+        node = TextNode(
+            "Here is a link [link text](https://test.com) with some end text",
+            TextType.TEXT,
+        )
+        old_nodes = [node]
+        expected = [
+            TextNode("Here is a link ", TextType.TEXT),
+            TextNode("link text", TextType.LINK, "https://test.com"),
+            TextNode(" with some end text", TextType.TEXT),
+        ]
+        result = split_nodes_link(old_nodes)
+        self.assertEqual(result, expected)
+
+    def test_link_mult(self):
+        node = TextNode(
+            "Here is a link [l1](url1) with another [l2](url2)",
+            TextType.TEXT,
+        )
+        old_nodes = [node]
+        expected = [
+            TextNode("Here is a link ", TextType.TEXT),
+            TextNode("l1", TextType.LINK, "url1"),
+            TextNode(" with another ", TextType.TEXT),
+            TextNode("l2", TextType.LINK, "url2"),
+        ]
+        result = split_nodes_link(old_nodes)
+        self.assertEqual(result, expected)
+
+    def test_link_with_image(self):
+        node = TextNode(
+            "Here is a link [l1](url1) with an image ![l2](url2)",
+            TextType.TEXT,
+        )
+        old_nodes = [node]
+        expected = [
+            TextNode("Here is a link ", TextType.TEXT),
+            TextNode("l1", TextType.LINK, "url1"),
+            TextNode(" with an image ![l2](url2)", TextType.TEXT),
+        ]
+        result = split_nodes_link(old_nodes)
+        self.assertEqual(result, expected)
+
+    def test_weird_link(self):
+        node = TextNode(
+            "Here is a weird link ![kin[l1](url1)",
+            TextType.TEXT,
+        )
+        old_nodes = [node]
+        expected = [
+            TextNode("Here is a weird link ![kin", TextType.TEXT),
+            TextNode("l1", TextType.LINK, "url1"),
+        ]
+        result = split_nodes_link(old_nodes)
+        self.assertEqual(result, expected)
+
+    def test_weird_link2(self):
+        node = TextNode(
+            "Here is a weird link [kin[l1](url1)",
+            TextType.TEXT,
+        )
+        old_nodes = [node]
+        expected = [
+            TextNode("Here is a weird link [kin", TextType.TEXT),
+            TextNode("l1", TextType.LINK, "url1"),
+        ]
+        result = split_nodes_link(old_nodes)
+        self.assertEqual(result, expected)
+
+    def test_img(self):
+        node = TextNode(
+            "Here is an image ![image text](https://imageurl.com) with some end text",
+            TextType.TEXT,
+        )
+        old_nodes = [node]
+        expected = [
+            TextNode("Here is an image ", TextType.TEXT),
+            TextNode("image text", TextType.IMAGE, "https://imageurl.com"),
+            TextNode(" with some end text", TextType.TEXT),
+        ]
+        result = split_nodes_image(old_nodes)
+        self.assertEqual(result, expected)
+
+    def test_img_mult(self):
+        node = TextNode(
+            "Here is an image ![t1](url1) and another ![t2](url2)", TextType.TEXT
+        )
+        old_nodes = [node]
+        expected = [
+            TextNode("Here is an image ", TextType.TEXT),
+            TextNode("t1", TextType.IMAGE, "url1"),
+            TextNode(" and another ", TextType.TEXT),
+            TextNode("t2", TextType.IMAGE, "url2"),
+        ]
+        result = split_nodes_image(old_nodes)
+        self.assertEqual(result, expected)
+
+    def test_img_mult_both_ends(self):
+        node = TextNode(
+            "![t3](url3) Here is an image ![t1](url1) and another ![t2](url2)",
+            TextType.TEXT,
+        )
+        old_nodes = [node]
+        expected = [
+            TextNode("t3", TextType.IMAGE, "url3"),
+            TextNode(" Here is an image ", TextType.TEXT),
+            TextNode("t1", TextType.IMAGE, "url1"),
+            TextNode(" and another ", TextType.TEXT),
+            TextNode("t2", TextType.IMAGE, "url2"),
+        ]
+        result = split_nodes_image(old_nodes)
+        self.assertEqual(result, expected)
+
+    def test_img_with_link(self):
+        node = TextNode(
+            "Here is an image ![t1](url1) and a link [anchor text](url2)",
+            TextType.TEXT,
+        )
+        old_nodes = [node]
+        expected = [
+            TextNode("Here is an image ", TextType.TEXT),
+            TextNode("t1", TextType.IMAGE, "url1"),
+            TextNode(" and a link [anchor text](url2)", TextType.TEXT),
+        ]
+        result = split_nodes_image(old_nodes)
+        self.assertEqual(result, expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
