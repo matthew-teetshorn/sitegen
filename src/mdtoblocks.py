a8d7251a54110fc219568ad9262c52033486d27d
matthew-teetshorn <matthew.teetshorn@gmail.com>
2025-11-16 18:24:52 -0600

    Added markdown identification by block type

--- src/mdtoblocks.py
+++ src/mdtoblocks.py
@@ -1,3 +1,16 @@
+from enum import Enum
+import re
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+
 def markdown_to_blocks(string: str) -> list[str]:
     strings = string.split("\n\n")
 
@@ -7,3 +20,80 @@ def markdown_to_blocks(string: str) -> list[str]:
     strings = [keep for keep in strings if keep != remove]
 
     return strings
+
+
+def block_is_heading(block: str) -> bool:
+    # Supported heading style 1-6 '#' with <space> after
+    REGEX_H = r"^\#{1,6}\s"
+
+    # Currently only support headings on single lines with newlines surrounding
+    if "\n" in block or re.findall(REGEX_H, block) == []:
+        return False
+
+    return True
+
+
+def block_is_code(block: str) -> bool:
+    # Supported code block style ``` stuff ```
+    REGEX_C = r"^`{3}.*`{3}$"
+
+    # Code blocks can contain newline sequences
+    if re.findall(REGEX_C, block, flags=re.DOTALL) == []:
+        return False
+
+    return True
+
+
+def block_is_quote(block: str) -> bool:
+    # Supported quote symbol: > with <space> after
+    REGEX_Q = r"^\> "
+
+    substrings = block.split("\n")
+    for substring in substrings:
+        if re.search(REGEX_Q, substring) is None:
+            return False
+
+    return True
+
+
+def block_is_ul(block: str) -> bool:
+    # Supported unordered list symbol: '-' with <space> after
+    REGEX_UL = r"^\- "
+
+    substrings = block.split("\n")
+    for substring in substrings:
+        if re.search(REGEX_UL, substring) is None:
+            return False
+
+    return True
+
+
+def block_is_ol(block: str) -> bool:
+    # Supported ordered list symbol: 'N' with <space> after
+    # N must start at '1' and increment by 1 on each substring
+    REGEX_OL = r"^(\d+)\. "
+
+    substrings = block.split("\n")
+    start_num = 1
+    for substring in substrings:
+        regex_result = re.findall(REGEX_OL, substring)
+        if regex_result == [] or int(regex_result[0]) != start_num:
+            return False
+        start_num += 1
+
+    return True
+
+
+def block_to_blocktype(block: str) -> BlockType:
+    if block_is_heading(block):
+        return BlockType.HEADING
+    if block_is_code(block):
+        return BlockType.CODE
+    if block_is_quote(block):
+        return BlockType.QUOTE
+    if block_is_ul(block):
+        return BlockType.UNORDERED_LIST
+    if block_is_ol(block):
+        return BlockType.ORDERED_LIST
+
+    return BlockType.PARAGRAPH
--- src/test_mdtoblocks.py
+++ src/test_mdtoblocks.py
@@ -1,7 +1,7 @@
 import unittest
 import os
 
-from mdtoblocks import markdown_to_blocks
+from mdtoblocks import block_to_blocktype, markdown_to_blocks, BlockType
 
 
 class TestSplitImgsLinks(unittest.TestCase):
@@ -23,8 +23,6 @@ This is the same paragraph on a new line
 - with items
 """
         blocks = markdown_to_blocks(md)
-        print("\n:::::::::::::::::::RESULT INCOMING::::::::::::::::::")
-        print(blocks)
         self.assertEqual(
             blocks,
             [
@@ -47,8 +45,6 @@ this is a paragraph
 with a bunch of lines
 """
         blocks = markdown_to_blocks(md)
-        print("\n:::::::::::::::::::RESULT INCOMING::::::::::::::::::")
-        print(blocks)
         self.assertEqual(
             blocks,
             [
@@ -70,8 +66,6 @@ Heres one in the middle
 with a bunch of lines
 """
         blocks = markdown_to_blocks(md)
-        print("\n:::::::::::::::::::RESULT INCOMING::::::::::::::::::")
-        print(blocks)
         self.assertEqual(
             blocks,
             [
@@ -80,6 +74,169 @@ with a bunch of lines
                 "with a bunch of lines",
             ],
         )
+
+    def test_p4(self):
+        md = """
+# this is a heading
+### with another heading
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "# this is a heading\n### with another heading",
+            ],
+        )
+
+    def test_h1(self):
+        md = "# Heading"
+        expected = BlockType.HEADING
+        result = block_to_blocktype(md)
+        self.assertEqual(result, expected)
+
+    def test_h2(self):
+        md = "###### Heading"
+        expected = BlockType.HEADING
+        result = block_to_blocktype(md)
+        self.assertEqual(result, expected)
+
+    def test_h3(self):
+        # Too many #s
+        md = "####### Heading"
+        expected = BlockType.PARAGRAPH
+        result = block_to_blocktype(md)
+        self.assertEqual(result, expected)
+
+    def test_h4(self):
+        # Only support blocks with single heading lines
+        md = "# Heading\n## Heading 2"
+        expected = BlockType.PARAGRAPH
+        result = block_to_blocktype(md)
+        self.assertEqual(result, expected)
+
+    def test_h5(self):
+        # Must have space between
+        md = "#Heading"
+        expected = BlockType.PARAGRAPH
+        result = block_to_blocktype(md)
+        self.assertEqual(result, expected)
+
+    def test_c1(self):
+        md = "``` This is a code block ```"
+        expected = BlockType.CODE
+        result = block_to_blocktype(md)
+        self.assertEqual(result, expected)
+
+    def test_c2(self):
+        md = """```
+int max = 10;
+for (int i = 0; i < max; i++) {
+doSomething;
+}
+```"""
+        expected = BlockType.CODE
+        result = block_to_blocktype(md)
+        self.assertEqual(result, expected)
+
+    def test_c3(self):
+        md = """```
+int max = 10;
+for (int i = 0; i < max; i++) {
+doSomething;
+}
+`"""
+        # Improper ending "`" for code block
+        expected = BlockType.PARAGRAPH
+        result = block_to_blocktype(md)
+        self.assertEqual(result, expected)
+
+    def test_q1(self):
+        md = "> Here is a block quote"
+        expected = BlockType.QUOTE
+        result = block_to_blocktype(md)
+        self.assertEqual(result, expected)
+
+    def test_q2(self):
+        md = "> Here is a block quote\n> Another\n> Yet Another\n> Yup, again"
+        expected = BlockType.QUOTE
+        result = block_to_blocktype(md)
+        self.assertEqual(result, expected)
+
+    def test_q3(self):
+        # Missing space at beginning
+        md = ">Here is a block quote"
+        expected = BlockType.PARAGRAPH
+        result = block_to_blocktype(md)
+        self.assertEqual(result, expected)
+
+    def test_q4(self):
+        md = "> Here is a block quote\n> Another\n> Yet Another\n>nope"
+        expected = BlockType.PARAGRAPH
+        result = block_to_blocktype(md)
+        self.assertEqual(result, expected)
+
+    def test_ul1(self):
+        md = "- Here is an unordered list"
+        expected = BlockType.UNORDERED_LIST
+        result = block_to_blocktype(md)
+        self.assertEqual(result, expected)
+
+    def test_ul2(self):
+        md = "- Here is a unordered list\n- Another\n- Yet Another\n- Yup, again"
+        expected = BlockType.UNORDERED_LIST
+        result = block_to_blocktype(md)
+        self.assertEqual(result, expected)
+
+    def test_ul3(self):
+        # Missing space at beginning
+        md = ">Here is a bad list"
+        expected = BlockType.PARAGRAPH
+        result = block_to_blocktype(md)
+        self.assertEqual(result, expected)
+
+    def test_ul4(self):
+        # One line not quoted
+        md = "- Here is a unordered list\n- Another\noops\n- Yup, again"
+        expected = BlockType.PARAGRAPH
+        result = block_to_blocktype(md)
+        self.assertEqual(result, expected)
+
+    def test_ol1(self):
+        md = "1. Here is an ordered list"
+        expected = BlockType.ORDERED_LIST
+        result = block_to_blocktype(md)
+        self.assertEqual(result, expected)
+
+    def test_ol2(self):
+        md = "2. Here is an ordered list"
+        expected = BlockType.PARAGRAPH
+        result = block_to_blocktype(md)
+        self.assertEqual(result, expected)
+
+    def test_ol3(self):
+        md = "1. Here is an ordered list\n2. Next item\n3. Third item"
+        expected = BlockType.ORDERED_LIST
+        result = block_to_blocktype(md)
+        self.assertEqual(result, expected)
+
+    def test_ol4(self):
+        md = "10. Here is an ordered list"
+        expected = BlockType.PARAGRAPH
+        result = block_to_blocktype(md)
+        self.assertEqual(result, expected)
+
+    def test_ol5(self):
+        md = "1. a\n2. a\n3. a\n4. a\n5. a\n6. a\n7. a\n8. a\n9. a\n10. a"
+        expected = BlockType.ORDERED_LIST
+        result = block_to_blocktype(md)
+        self.assertEqual(result, expected)
+
+    def test_ol6(self):
+        # Misnumbered at entry 5
+        md = "1. a\n2. a\n3. a\n4. a\n4. a\n6. a\n7. a\n8. a\n9. a\n10. a"
+        expected = BlockType.PARAGRAPH
+        result = block_to_blocktype(md)
+        self.assertEqual(result, expected)
 
 
 if __name__ == "__main__":
